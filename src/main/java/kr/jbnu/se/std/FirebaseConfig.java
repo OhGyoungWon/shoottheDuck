1a877b0e9221de88f60f19a6aafbf680492aba0c
OhGyoungWon <ruddnjs5@jbnu.ac.kr>
2024-10-22 23:20:37 +0900

    리얼 마지막의 마지막의 마지막

--- src/main/java/kr/jbnu/se/std/FirebaseConfig.java
+++ src/main/java/kr/jbnu/se/std/FirebaseConfig.java
@@ -16,11 +16,11 @@ public class FirebaseConfig {
             return; // 초기화가 이미 되어 있다면 메소드 종료
         }
         //만약에 파일 경로로 문제가 생긴다면  Admin SDK key의 파일 절대 경로를 넣을 것
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/stduck-252fa-firebase-adminsdk-896br-1ab074317d.json");
+        FileInputStream serviceAccount = new FileInputStream("path/to/Admin-SDK-Key.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://stduck-252fa-default-rtdb.firebaseio.com")  //오경원 realtime base url
+                .setDatabaseUrl("https://your-project.firebaseio.com")  //realtime base url
                 .build();
 
         // FirebaseApp이 이미 초기화되어 있는지 확인
