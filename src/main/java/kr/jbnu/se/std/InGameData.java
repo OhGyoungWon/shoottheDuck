2f41d0901766e67ef588c01136b5f0e64c63a5ae
pcy79 <pcy7952@daum.net>
2024-12-03 09:28:02 +0900

    일부 변수 private화

--- src/main/java/kr/jbnu/se/std/InGameData.java
+++ src/main/java/kr/jbnu/se/std/InGameData.java
@@ -12,6 +12,7 @@ public final class InGameData {
     private static final FirebaseDatabase db = FirebaseDatabase.getInstance();
     private static final Logger log = LoggerFactory.getLogger(InGameData.class);
     private static final DatabaseReference usersRef = db.getReference("users/");
+    private static final String USER_INFO_PATH = "userInfo/";
     private static int score;
 
     private InGameData(){
@@ -28,21 +29,21 @@ public final class InGameData {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // 현재 사용자의 topScore를 가져옴
-                Integer topScore = dataSnapshot.child("userInfo/" + sanitizedEmail + "/topScore").getValue(Integer.class);
-                String nickname = dataSnapshot.child("userInfo/" + sanitizedEmail + "/nickname").getValue(String.class);
+                Integer topScore = dataSnapshot.child(USER_INFO_PATH + sanitizedEmail + "/topScore").getValue(Integer.class);
+                String nickname = dataSnapshot.child(USER_INFO_PATH + sanitizedEmail + "/nickname").getValue(String.class);
                 Map<String, Object> userUpdates = new HashMap<>();
 
                 // topScore와 currentScore 비교
                 if (topScore == null || score > topScore) {
                     // currentScore가 topScore보다 크면 topScore를 currentScore로 업데이트
-                    userUpdates.put("userInfo/" + sanitizedEmail + "/topScore", score);
-                    userUpdates.put("userInfo/" + sanitizedEmail + "/currentScore", score);
+                    userUpdates.put(USER_INFO_PATH + sanitizedEmail + "/topScore", score);
+                    userUpdates.put(USER_INFO_PATH + sanitizedEmail + "/currentScore", score);
                     userUpdates.put("leaderboard/" + nickname, score);
 
                     usersRef.updateChildrenAsync(userUpdates);
                 } else {
                     // currentScore는 그대로 저장
-                    userUpdates.put("userInfo/" + sanitizedEmail + "/currentScore", score);
+                    userUpdates.put(USER_INFO_PATH + sanitizedEmail + "/currentScore", score);
 
                     usersRef.updateChildrenAsync(userUpdates);
 
